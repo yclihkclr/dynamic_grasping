426ea81b92744b006d6ea2a62b4b8b99be468b80
yclihkclr <ycli@hkclr>
2023-03-02 20:00:41 +0800

    resolve merge conflict

--- src/franka_motion/src/RobotNode.cpp
+++ src/franka_motion/src/RobotNode.cpp
@@ -11,25 +11,18 @@ void spin(std::shared_ptr<rclcpp::executors::MultiThreadedExecutor> exe)
 
 int main(int argc, char** argv) {
 
-<<<<<<< HEAD
     //set use_franka_hand or not
     bool use_franka_hand = true;
-=======
->>>>>>> 33994516c9e18c65880689f1e064c03fd2d2679e
 
     // Init ROS node
     rclcpp::init(argc, argv);
     auto node = rclcpp::Node::make_shared("franka_server_node");
-<<<<<<< HEAD
 
     node->declare_parameter<bool>("hand", 1);
     node->get_parameter<bool>("hand", use_franka_hand);
     RCLCPP_INFO(node->get_logger(), "use_franka_hand = %d", use_franka_hand);
 
     auto server = std::make_shared<RobotServer>(node,use_franka_hand);
-=======
-    auto server = std::make_shared<RobotServer>(node);
->>>>>>> 33994516c9e18c65880689f1e064c03fd2d2679e
 
     rclcpp::executor::ExecutorArgs arg;
     auto executor = std::make_shared<rclcpp::executors::MultiThreadedExecutor>(arg, 2);
--- src/franka_motion/src/RobotServer.cpp
+++ src/franka_motion/src/RobotServer.cpp
@@ -3,21 +3,14 @@
 const std::string cart_motion_time_srv_id = "/franka_motion/cart_motion_time";
 const std::string joint_motion_vel_srv_id = "/franka_motion/joint_motion_vel";
 const std::string pose_path_srv_id = "/franka_motion/pose_path";
-<<<<<<< HEAD
 const std::string franka_hand_srv_id = "/franka_motion/franka_hand";
 const std::string franka_fci_ip = "192.168.1.20";
 
 RobotServer::RobotServer(std::shared_ptr<rclcpp::Node> node, bool use_franka_hand) {
-=======
-const std::string franka_fci_ip = "192.168.1.20";
-
-RobotServer::RobotServer(std::shared_ptr<rclcpp::Node> node) {
->>>>>>> 33994516c9e18c65880689f1e064c03fd2d2679e
     node_ = node;
 
     // Initialize scaling velocity and acceleration service
     cartMotionTimeService_ = node->create_service<franka_interfaces::srv::CartMotionTime>(cart_motion_time_srv_id, std::bind(&RobotServer::cartMotionTimeSrvCb, this, std::placeholders::_1, std::placeholders::_2));
-<<<<<<< HEAD
     RCLCPP_INFO(rclcpp::get_logger("robot_server"), "Advertising service %s", cart_motion_time_srv_id.c_str());
 
     jointMotionVelService_ = node->create_service<franka_interfaces::srv::JointMotionVel>(joint_motion_vel_srv_id, std::bind(&RobotServer::jointMotionVelSrvCb, this, std::placeholders::_1, std::placeholders::_2));
@@ -70,36 +63,13 @@ bool RobotServer::frankaHandSrvCb(const std::shared_ptr<franka_interfaces::srv::
     res->success = result;
     return true;
 }
-=======
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Advertising service %s", cart_motion_time_srv_id.c_str());
-
-    jointMotionVelService_ = node->create_service<franka_interfaces::srv::JointMotionVel>(joint_motion_vel_srv_id, std::bind(&RobotServer::jointMotionVelSrvCb, this, std::placeholders::_1, std::placeholders::_2));
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Advertising service %s", joint_motion_vel_srv_id.c_str());
-
-    posePathService_ = node->create_service<franka_interfaces::srv::PosePath>(pose_path_srv_id, std::bind(&RobotServer::posePathBezierCb, this, std::placeholders::_1, std::placeholders::_2));
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Advertising service %s", pose_path_srv_id.c_str());
-
-    robot_ = std::make_shared<orl::Robot>(franka_fci_ip);
-
-    // init pose
-    std::array<double, 7> q_goal = {{-0.00771968,-0.599246,-0.0852301,-2.62207,-0.0888355,2.13279,0.693285}};
-    robot_->joint_motion(q_goal, 0.3); 
-}
-
->>>>>>> 33994516c9e18c65880689f1e064c03fd2d2679e
 
 bool RobotServer::cartMotionTimeSrvCb(const std::shared_ptr<franka_interfaces::srv::CartMotionTime::Request> req,
            std::shared_ptr<franka_interfaces::srv::CartMotionTime::Response> res)
 {
-<<<<<<< HEAD
     RCLCPP_INFO(rclcpp::get_logger("robot_server"), "Pose [%f,%f,%f,%f,%f,%f]",req->pose[0],req->pose[1],req->pose[2],req->pose[3],req->pose[4],req->pose[5]);
     RCLCPP_INFO(rclcpp::get_logger("robot_server"), "Duration %f",req->duration);
     RCLCPP_INFO(rclcpp::get_logger("robot_server"), "cartMotionTimeSrvCb");
-=======
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Pose [%f,%f,%f,%f,%f,%f]",req->pose[0],req->pose[1],req->pose[2],req->pose[3],req->pose[4],req->pose[5]);
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Duration %f",req->duration);
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "cartMotionTimeSrvCb");
->>>>>>> 33994516c9e18c65880689f1e064c03fd2d2679e
 
     orl::Pose dest_pose({req->pose[0],req->pose[1],req->pose[2]},{req->pose[3],req->pose[4],req->pose[5]});
     robot_->cart_motion(dest_pose, req->duration);
@@ -116,15 +86,9 @@ bool RobotServer::cartMotionTimeSrvCb(const std::shared_ptr<franka_interfaces::s
 bool RobotServer::jointMotionVelSrvCb(const std::shared_ptr<franka_interfaces::srv::JointMotionVel::Request> req,
            std::shared_ptr<franka_interfaces::srv::JointMotionVel::Response> res)
 {
-<<<<<<< HEAD
     RCLCPP_INFO(rclcpp::get_logger("robot_server"), "Joint [%f,%f,%f,%f,%f,%f,%f]",req->joints[0],req->joints[1],req->joints[2],req->joints[3],req->joints[4],req->joints[5],req->joints[6]);
     RCLCPP_INFO(rclcpp::get_logger("robot_server"), "Velocity Scaling %f",req->velscale);
     RCLCPP_INFO(rclcpp::get_logger("robot_server"), "jointMotionVelSrvCb");
-=======
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Joint [%f,%f,%f,%f,%f,%f,%f]",req->joints[0],req->joints[1],req->joints[2],req->joints[3],req->joints[4],req->joints[5],req->joints[6]);
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Velocity Scaling %f",req->velscale);
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "jointMotionVelSrvCb");
->>>>>>> 33994516c9e18c65880689f1e064c03fd2d2679e
     std::array<double, 7> q_goal = {{req->joints[0],req->joints[1],req->joints[2],req->joints[3],req->joints[4],req->joints[5],req->joints[6]}};
     robot_->joint_motion(q_goal, req->velscale); 
 
@@ -145,17 +109,10 @@ bool RobotServer::posePathBezierCb(const std::shared_ptr<franka_interfaces::srv:
     {
       orl::Position pose(req->poses[0+i*3],req->poses[1+i*3],req->poses[2+i*3]);
       way_points.push_back(pose);
-<<<<<<< HEAD
       RCLCPP_INFO(rclcpp::get_logger("robot_server"), "Waypoint %d with Pose [%f,%f,%f]",i+1,req->poses[0+i*3],req->poses[1+i*3],req->poses[2+i*3]);
     }
     RCLCPP_INFO(rclcpp::get_logger("robot_server"), "Duration %f",req->duration);
     RCLCPP_INFO(rclcpp::get_logger("robot_server"), "posePathSrvCb");
-=======
-      RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Waypoint %d with Pose [%f,%f,%f]",i+1,req->poses[0+i*3],req->poses[1+i*3],req->poses[2+i*3]);
-    }
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Duration %f",req->duration);
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "posePathSrvCb");
->>>>>>> 33994516c9e18c65880689f1e064c03fd2d2679e
 
     auto bezier_movement = orl::PoseGenerators::BezierMotion(way_points);
     orl::apply_speed_profile(bezier_movement, orl::SpeedProfiles::QuinticPolynomialProfile());
